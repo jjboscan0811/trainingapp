66562c6f64c0d238b69c487050db4b2e14692642
jjboscan0811 <jjboscan@hotmail.com>
2024-08-22 13:52:37 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return 'About Page'
+
+if __name__ == '__main__':
+    app.run(debug=True)
